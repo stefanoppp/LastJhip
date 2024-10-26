0faacf83210cba0a11e85f012f25de2cd44522f5
Stefano <spstefanoperetti17@gmail.com>
2024-10-26 12:17:42 -0300

    Queda formatear JSON de front

--- src/main/java/um/prog2/repository/OpcionRepository.java
+++ src/main/java/um/prog2/repository/OpcionRepository.java
@@ -3,10 +3,15 @@ package um.prog2.repository;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 import um.prog2.domain.Opcion;
+import java.util.List;
 
 /**
  * Spring Data JPA repository for the Opcion entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface OpcionRepository extends JpaRepository<Opcion, Long> {}
+public interface OpcionRepository extends JpaRepository<Opcion, Long> {
+
+    // Método personalizado para obtener todas las opciones por el ID de la personalización
+    List<Opcion> findAllByPersonalizacionId(Long personalizacionId);
+}
